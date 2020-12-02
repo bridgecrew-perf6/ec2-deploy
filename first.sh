778f3dabaa9be5fd0e20981ca6b560ef5176b2ae
gattyan27 <radio.radirer@gmail.com>
2020-12-03 01:16:42 +0900

    push

--- first.sh
+++ first.sh
@@ -4,14 +4,14 @@ set -x
 # definitions
 export AWS_DEFAULT_REGION=ap-northeast-1
 
-region=$1
-userid=$2
-username=$3
-password=$4
-ssh_port=$5
-hosted_zone_id=$6
-domain=$7
-volume_id=$8
+region=ap-northeast-1
+userid=ichikawa
+username=ichikawa
+password=ichikawa
+ssh_port=22
+hosted_zone_id=Z08207963QLS1T1DCASQM
+domain=devservice.ml
+volume_id=0b0087720e5088eee
 
 instance_id=$(curl -s 169.254.169.254/latest/meta-data/instance-id)
 ip=$(curl -s 169.254.169.254/latest/meta-data/public-ipv4)
@@ -22,7 +22,7 @@ cd /home/ubuntu
 cp -p /etc/apt/sources.list /etc/apt/sources.list.bak
 sed -i 's/ap-northeast-1\.ec2\.//g' /etc/apt/sources.list
 apt update
-apt install nvme-cli
+apt install -y nvme-cli
 apt install -y unzip
 apt install -y python
 # DEBIAN_FRONTEND=noninteractive dpkg --configure -a --force-confdef --force-confnew
@@ -70,17 +70,6 @@ sed -e s/{%port%}/$ssh_port/g sshd_config.tmpl > sshd_config.init
 cp sshd_config.init /etc/ssh/sshd_config
 systemctl restart sshd
 
-# install nodejs
-apt install -y nodejs
-apt install -y npm
-
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-apt update
-apt install -y yarn
-npm install -g npx
-npm install -g typescript typescript-language-server
-yarn global add create-react-app
 
 # install docker
 sudo apt install -y \
@@ -106,8 +95,8 @@ systemctl restart docker
 
 # install nginx and certbot for let's encrypt
 cd /etc
-cp /home/$username/letsencrypt.tar.gz letsencrypt.tar.gz
-tar xzf letsencrypt.tar.gz
+# # cp /home/$username/letsencrypt.tar.gz letsencrypt.tar.gz
+# tar xzf letsencrypt.tar.gz
 cd /home/ubuntu
 
 apt install -y nginx
@@ -115,11 +104,11 @@ curl https://raw.githubusercontent.com/gattyan27/ec2-deploy/master/http.conf.tmp
 sed -e s/{%domain%}/$domain/g http.conf.tmpl > http.conf
 cp http.conf /etc/nginx/conf.d/http.conf
 
-apt install -y software-properties-common
-add-apt-repository -y universe
-add-apt-repository -y ppa:certbot/certbot
-apt update
-apt install -y certbot python-certbot-nginx
+# apt install -y software-properties-common
+# add-apt-repository -y universe
+# add-apt-repository -y ppa:certbot/certbot
+# apt update
+# apt install -y certbot python-certbot-nginx
 
 # install library for puppeteer
 # apt install -y gconf-service libasound2 libatk1.0-0 libatk-bridge2.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
